c7593b363c1492cec921fc447264022bd48b608d
user test <klimchuk.alieksandr.1987@mail.ru>
2022-12-24 02:19:01 +0300

    first commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int TicketPrice = 12_850;
+        int bonus = TicketPrice / 20;
+        System.out.print("Бонус: " + bonus);
+    }
+}
